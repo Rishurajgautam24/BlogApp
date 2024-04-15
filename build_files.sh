9c339ba44e46a83fdd11a4b6475ef6b9be7faaea
RISHU RAJ GAUTAM <108501074+Rishurajgautam24@users.noreply.github.com>
2024-04-15 19:16:21 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
 echo "BUILD STARTED"
-sudo apt update
-sudo apt install python3 python3-pip
+
+apt update
+apt install -y python3 python3-pip
+
 python3.9 -m pip install -r requirements.txt
+
+
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD ENDED"
-
-
-
