0972fe54693d53df0234010c86f41c5132d390d0
RISHU RAJ GAUTAM <108501074+Rishurajgautam24@users.noreply.github.com>
2024-04-15 19:05:34 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-
+echo "BUILD STARTED"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD ENDED"
 
 
 
